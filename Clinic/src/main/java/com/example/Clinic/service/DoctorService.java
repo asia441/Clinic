c867cf6d21d6c57076ba210d48fde7bd090b5339
Joanna Humnicka <j.humnicka@gmail.com>
2019-05-25 09:48:23 +0200

    DoctorRepository, DoctorService

--- Clinic/src/main/java/com/example/Clinic/service/DoctorService.java
+++ Clinic/src/main/java/com/example/Clinic/service/DoctorService.java
@@ -1,4 +1,25 @@
 package com.example.Clinic.service;
 
+import com.example.Clinic.model.Doctor;
+import com.example.Clinic.repository.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+@Component
 public class DoctorService {
+
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    public Doctor save(Doctor doctor){
+        return doctorRepository.save(doctor);
+    }
+    public Iterable<Doctor> findAll() {
+        return doctorRepository.findAll();
+    }
+
+    public Optional<Doctor> findById(Long patientId) {
+        return  doctorRepository.findById(patientId);
+    }
 }
