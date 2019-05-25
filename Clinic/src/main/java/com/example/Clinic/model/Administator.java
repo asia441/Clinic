b864b39dc86e1f625fb3616e63d417dd286e7c2d
Przemyslaw Bryda <przemyslaw.bryda@gmail.com>
2019-05-25 09:26:07 +0200

    Method add

--- Clinic/src/main/java/com/example/Clinic/model/Administator.java
+++ Clinic/src/main/java/com/example/Clinic/model/Administator.java
@@ -5,10 +5,46 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
 @NoArgsConstructor
 public class Administator extends User {
 
+    @Override
+    public void logIn() {
+        super.logIn();
+    }
+
+    @Override
+    public void logOut() {
+        super.logOut();
+    }
+
+    @Override
+    public List<Doctor> ShowAllDoctors() {
+        return super.ShowAllDoctors();
+    }
+
+    @Override
+    public void showDoctorSchedule() {
+        super.showDoctorSchedule();
+    }
+
+    public Patient showPatientData(Patient patient) {
+        return null;
+    }
+
+    public List<Office> showOffices() {
+        List<Office> officesList = new ArrayList<>();
+        return officesList;
+    }
+
+    public void assignDoctorToOffice(Doctor doctor) {}
+
+    public void setScheduleForDoctor() {}
+
+    public void setVisitForPatient() {}
 }
